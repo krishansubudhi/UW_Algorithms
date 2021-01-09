8f7e7505a60e065cc8c9a4018e7cdfed39ef096d
Krishan Subudhi <krkusuk@microsoft.com>
2021-01-09 11:15:55 -0800

    quick select complete

---
+++ HW1/question4/quickselect.py
@@ -0,0 +1,97 @@
+'''
+Implement the randomized selection algorithm from class, where the pivot is chosen at random
+from the array. What is the expected number of comparisons that you do between data elements
+to find the median?
+'''
+import random
+class QuickSelect:
+
+    def kth_largest(self, data :list, k: int):
+        self.data = data
+        random.seed(None)
+        self.comparison_count = 0
+        self.partition_count = 0
+        assert k<=len(self.data) and k>0, 'K out of range'
+        element =  self._kth_largest(k, 0, len(data)-1)
+        return element
+    
+    def _kth_largest(self, k: int, start : int, end : int):
+        """
+        Given a list, find it's kth largest element using Quick select algorithm
+        """
+        # 1. select a random pivot
+        pivotIndex = self.get_pivot_index(start, end)
+        # print('Before partition start, end, pivotIndex ',  start, end, pivotIndex)
+        pivotIndex = self.partition(pivotIndex, start, end)
+        # print(f'After partition , data = {data}, pivotIndex = {pivotIndex}')
+
+        if end-pivotIndex >=k:
+            return self._kth_largest(k, pivotIndex+1, end)
+            
+        elif end-pivotIndex+1 == k:
+            return self.data[pivotIndex]
+        else:
+            return self._kth_largest(k-1-(end-pivotIndex), start , pivotIndex-1)
+
+    def partition(self, pivotIndex, start, end):
+        self.partition_count += 1
+        pivotValue = self.data[pivotIndex]
+        # Move pivot to right
+        self.swap(end , pivotIndex)
+        pivotIndex = end
+        leftIndex = start
+        # Loop till end-1.
+        for i in range(start, end):
+            self.comparison_count += 1
+            if data[i] < pivotValue:
+                self.swap(leftIndex, i)
+                leftIndex += 1
+        self.swap(leftIndex, pivotIndex)
+        return leftIndex
+    
+    def swap(self, i , j):
+        t = self.data[j]
+        self.data[j] = self.data[i]
+        self.data[i] = t
+
+    def get_pivot_index(self,start, end):
+        randomIndex = random.randint(start,end)
+        return randomIndex
+
+    def __str__(self):
+        return 'Quick select random pivot'
+
+class QuickSelectMedian(QuickSelect):
+    def __init__(self,median_of = 3):
+        self.median_of = median_of
+    def get_pivot_index(self, start, end):
+        randomIndex = [random.randint(start,end) for _ in range(self.median_of)]
+        # print(randomIndex)
+        randomIndex = sorted(randomIndex, key = lambda index: self.data[index])
+        median = randomIndex[self.median_of//2]
+        # print(randomIndex)
+        return median
+
+    def __str__(self):
+        return f'Quick select Median of {self.median_of}'
+
+if __name__ == '__main__':
+    import sys
+    algos = [QuickSelect(), QuickSelectMedian(3), QuickSelectMedian(5)]
+    iterations  =100
+    n = 100000
+    for q in algos:
+        print('\n',q)
+        comparisons = 0
+        partitions = 0
+        for _ in range(iterations):
+            data = [i for i in range(1,n)]
+            k = len(data)//2+1 #median
+            element = q.kth_largest(data, k)
+            comparisons += q.comparison_count
+            partitions += q.partition_count
+        print(f'Average partitions per run = {partitions/iterations}')
+        print(f'Average comparisons per run = {comparisons/iterations} = {comparisons/iterations/len(data)} n')
+
+
+
---
+++ HW1/question4/readme.md
@@ -0,0 +1,12 @@
+python .\quickselect.py
+ Quick select random pivot
+Average partitions per run = 21.67
+Average comparisons per run = 344782.41 = 3.4478585785857856 n
+
+ Quick select Median of 3
+Average partitions per run = 19.2
+Average comparisons per run = 282551.91 = 2.8255473554735544 n
+
+ Quick select Median of 5
+Average partitions per run = 17.61
+Average comparisons per run = 249618.57 = 2.496210662106621 n

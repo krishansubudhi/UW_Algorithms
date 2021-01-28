f8cc017af6a59ce8aa41482a2f4211d8ab368b7b
Krishan Subudhi <krkusuk@microsoft.com>
2021-01-27 21:40:04 -0800

    finished hw3

--- HW3/excel_to_latex.py
+++ HW3/excel_to_latex.py
@@ -1,5 +1,5 @@
 import pandas as pd
-q5 = pd.read_excel('q5_complexities.xls', sheet_name='q5')
-q5 = q5.set_index('n')
-latex = q5.to_latex(caption = 'Question 5 results',label='table:q5')
+q6 = pd.read_excel('q5_complexities.xls', sheet_name='q6')
+q6 = q6.set_index('n')
+latex = q6.to_latex(caption = 'Question 6 results',label='table:q6')
 print(latex)

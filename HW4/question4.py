50f43b4d5ad3df26a64d6324154c6252994a7cff
Krishan Subudhi <krkusuk@microsoft.com>
2021-01-31 12:41:23 -0800

    HW4 programming

---
+++ HW4/question4.py
@@ -0,0 +1,75 @@
+'''
+#Algo
+Two choice hashing uses two hash function and assigns items to the least used bucket. 
+
+# Assumptions
+For this problem, you will assume a chaining implementation of hashing, since you are counting the number of elements assigned to buckets (but you don’t need to implement chaining, since you only need the counts.
+
+
+You may assume that your hash functions are perfectly random. 
+
+For each experiment you run, you should hash n items into n cells. 
+
+# Metric
+The statistic of most interest is the maximum number of items assigned to a cell, 
+
+but computing the standard deviation is also of interest.
+'''
+import random
+import numpy as np
+import logging
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+class KchoiceHashing():
+    def __init__(self, n, k):
+        self.n = n
+        self.k = k
+        # Buckets only store counts in this simulation.
+        self.buckets = [0] * self.n #nothing is assigned to the buckets yet
+        self.logger = logging.getLogger(name = __name__)
+        # random hash functions with different seeds.
+        # Each hash function is a random number generator.
+        self.h = [random.Random(i) for i in range(k)]
+
+    def assign_item(self, item = None):
+        '''Assigns a new item to a bucket.
+        For this simulation, we don't need to assign actual items.
+        We will just increment item count in the assigned bucket 
+        '''
+        # Compute hash functions and shortlist buckets
+        selected_buckets = [h.randint(0, n-1) for h in self.h]
+        current_items = np.array([self.buckets[b] for b in selected_buckets])
+        # select bucket with least amount of items in it
+        min_bucket = selected_buckets[np.argmin(current_items)]
+
+        self.logger.debug(f'selected bucket {min_bucket} from {selected_buckets}')
+
+        # assign item to that bucket
+        self.buckets[min_bucket] += 1
+
+        self.logger.debug(self.buckets)
+
+    def assign_n_items(self):
+        for i in range(n):
+            self.logger.debug(f'iteration {i}')
+            self.assign_item(i)
+    
+    def max_items(self):
+        return max(self.buckets)
+
+    def std(self):
+        return np.array(self.buckets).std()
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(name = __name__)
+
+    for n in [1_000,10_000,100_000,1_000_000]:
+        for k in [1,2,3]:
+            hashing = KchoiceHashing(n,k)
+            hashing.assign_n_items()
+            max_items = hashing.max_items()
+            ratio = max_items/np.log(np.log(n)) if n>1 else max_items/(np.log(n)/np.log(np.log(n)))
+            logger.info(f'n = {n}, k = {k}, max_items = {max_items}, max_items/loglogn = {ratio:.3}, std = {hashing.std():.3}')
+

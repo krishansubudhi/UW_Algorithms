587a0365160a099fe5401a4ced387713f5126fc0
Krishan Subudhi <krkusuk@microsoft.com>
2021-01-31 16:14:20 -0800

    question 1

---
+++ HW4/question1.py
@@ -0,0 +1,11 @@
+p = 1048579
+
+#2^(2^20+2) = 2^(2^20) + 2^2
+import math
+mods = {0:2}
+for i in range(1,21):
+    mod = (mods[i-1]*mods[i-1])%p
+    mods[i] = mod
+    print(f'2^(2^{i}) mod {p} = (2^(2^{i-1}) mod {p} * 2^(2^{i-1}) mod {p}) mod {p} = {mod}')
+
+print(f'2^(2^20+2) mod {p} = (2^(2^20) mod {p} * 2^(3) mod {p}) mod {p} = {(mods[20]*mods[1]) % p}')
